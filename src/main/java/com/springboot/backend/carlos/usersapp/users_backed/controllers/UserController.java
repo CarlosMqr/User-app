b4f5187be8c33be92fed6b91d2bd4a144c5e6ac7
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2026-06-23 20:35:34 -0600

    Se agrego la clase UserController y los metodos list y show

---
+++ src/main/java/com/springboot/backend/carlos/usersapp/users_backed/controllers/UserController.java
@@ -0,0 +1,52 @@
+package com.springboot.backend.carlos.usersapp.users_backed.controllers;
+
+import com.springboot.backend.carlos.usersapp.users_backed.model.User;
+import com.springboot.backend.carlos.usersapp.users_backed.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import static org.springframework.http.HttpStatus.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@CrossOrigin(origins = {"http://localhost:4200"})// para poder enviar y recibir datos
+@RestController// indicar que es una clase restcontroller
+@RequestMapping("/api")// aqui se genera la u
+public class UserController {
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+     @GetMapping("/users")
+    public List<User> list(){
+         return userServiceImpl.findAll();
+     }
+
+     @GetMapping("/users/{id}")
+    public ResponseEntity<User> show(@PathVariable Long id){
+         Map<String, Object> response = new HashMap<>();
+         Optional<User> userOptional;
+
+         try {
+              userOptional = userServiceImpl.findById(id);
+         }catch (DataAccessException e){
+             response.put("Mensaje", "Error al realizar la consulta en la base de datos");
+             response.put("error", e.getCause().getMessage());
+             return new ResponseEntity(response,INTERNAL_SERVER_ERROR);
+         }
+
+         if (userOptional.isPresent()){
+             return ResponseEntity.ok(userOptional.orElseThrow());
+         }else {
+             response.put("Mensaje", "el cliente con ID: ".concat(id.toString().concat(" no existe en la BD")));
+           return new ResponseEntity(response, NOT_FOUND);
+         }
+     }
+
+
+
+}
