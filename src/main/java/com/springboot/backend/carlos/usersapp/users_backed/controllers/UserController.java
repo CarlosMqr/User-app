027d44539a089b1aeb5036aba45896a3eefb2d24
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2026-06-24 10:34:22 -0600

    Se agrego en la clase UserController el metodo create

--- src/main/java/com/springboot/backend/carlos/usersapp/users_backed/controllers/UserController.java
+++ src/main/java/com/springboot/backend/carlos/usersapp/users_backed/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.springboot.backend.carlos.usersapp.users_backed.controllers;
 
 import com.springboot.backend.carlos.usersapp.users_backed.model.User;
 import com.springboot.backend.carlos.usersapp.users_backed.services.UserServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.HttpStatus.*;
 
@@ -13,6 +15,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = {"http://localhost:4200"})// para poder enviar y recibir datos
 @RestController// indicar que es una clase restcontroller
@@ -25,6 +28,7 @@ public class UserController {
     public List<User> list(){
          return userServiceImpl.findAll();
      }
+
 
      @GetMapping("/users/{id}")
     public ResponseEntity<User> show(@PathVariable Long id){
@@ -45,6 +49,32 @@ public class UserController {
              response.put("Mensaje", "el cliente con ID: ".concat(id.toString().concat(" no existe en la BD")));
            return new ResponseEntity(response, NOT_FOUND);
          }
+     }
+
+     @PostMapping("/users")
+    public ResponseEntity<User> create(@Valid @RequestBody User user, BindingResult result){
+     Map<String, Object> response = new HashMap<>();
+      User userC;
+
+      if (result.hasErrors()){
+          List<String> errors = result.getFieldErrors()
+                  .stream()
+                  .map(err -> "El campo  " + err.getField() + "' " + err.getDefaultMessage() )
+                  .collect(Collectors.toList());
+          response.put("errors", errors);
+          return new ResponseEntity(response,BAD_REQUEST);
+      }
+
+      try {
+          userC = userServiceImpl.save(user);
+      }catch (DataAccessException e){
+          response.put("Mensaje", "Error al realizar el INSERT en la BD");
+          response.put("Error", e.getCause().getMessage());
+          return  new ResponseEntity(response, INTERNAL_SERVER_ERROR);
+      }
+         response.put("Mensaje", "El usuario ha sido creado con éxito");
+         response.put("Usuario", userC);
+         return new ResponseEntity(response,CREATED);
      }
 
 
